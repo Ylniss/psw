8f5beb75cf4a6388801b0cd3dfa6bff306a55c84
Ylniss <zupqa0@gmail.com>
2024-02-17 21:44:59 +0100

    repeat ask for password on failed repeat

--- src/utils/prompts.go
+++ src/utils/prompts.go
@@ -2,13 +2,16 @@ package utils
 
 import (
 	"errors"
+	"fmt"
 	"os"
+
+	"github.com/TwiN/go-color"
 
 	"github.com/cqroot/prompt"
 	"github.com/cqroot/prompt/input"
 )
 
-var ErrPasswordsDontMatch = errors.New("Passwords don't match, try again")
+var passwordsDontMatchMsg = "Passwords don't match, try again"
 
 func validateMainPassLen(content string) error {
 	if len(content) < 4 {
@@ -19,56 +22,67 @@ func validateMainPassLen(content string) error {
 }
 
 func PromptForMainPass(ensure bool) (string, error) {
-	mainPass, err := prompt.New().Ask("Main password").
-		Input("", input.WithEchoMode(input.EchoPassword), input.WithValidateFunc(validateMainPassLen))
-	if err != nil {
-		if errors.Is(err, prompt.ErrUserQuit) {
-			os.Exit(1)
+	mainPass := "*"
+	repeatMainPass := ""
+
+	for mainPass != repeatMainPass { // ask until passwords match
+		var err error
+		mainPass, err = prompt.New().Ask("Main password").
+			Input("", input.WithEchoMode(input.EchoPassword), input.WithValidateFunc(validateMainPassLen))
+		if err != nil {
+			if errors.Is(err, prompt.ErrUserQuit) {
+				os.Exit(1)
+			}
+			return "", err
 		}
-		return "", err
-	}
 
-	if !ensure {
-		return mainPass, nil
-	}
-
-	repeatMainPass, err := prompt.New().Ask("Repeat main password").
-		Input("", input.WithEchoMode(input.EchoPassword), input.WithValidateFunc(validateMainPassLen))
-	if err != nil {
-		if errors.Is(err, prompt.ErrUserQuit) {
-			os.Exit(1)
+		if !ensure {
+			return mainPass, nil
 		}
-		return "", err
-	}
 
-	if mainPass != repeatMainPass {
-		return "", ErrPasswordsDontMatch
-	}
+		repeatMainPass, err = prompt.New().Ask("Repeat main password").
+			Input("", input.WithEchoMode(input.EchoPassword), input.WithValidateFunc(validateMainPassLen))
+		if err != nil {
+			if errors.Is(err, prompt.ErrUserQuit) {
+				os.Exit(1)
+			}
+			return "", err
+		}
 
+		if mainPass != repeatMainPass {
+			fmt.Println(color.InCyan(passwordsDontMatchMsg))
+		}
+	}
 	return mainPass, nil
 }
 
 func PromptForRecordPass() (string, error) {
-	recordPass, err := prompt.New().Ask("Password").
-		Input("", input.WithEchoMode(input.EchoPassword))
-	if err != nil {
-		if errors.Is(err, prompt.ErrUserQuit) {
-			os.Exit(1)
-		}
-		return "", err
-	}
+	recordPass := "*"
+	repeatRecordPass := ""
 
-	repeatRecordPass, err := prompt.New().Ask("Repeat password").
-		Input("", input.WithEchoMode(input.EchoPassword))
-	if err != nil {
-		if errors.Is(err, prompt.ErrUserQuit) {
-			os.Exit(1)
+	for recordPass != repeatRecordPass { // ask until passwords match
+		var err error
+		recordPass, err = prompt.New().Ask("Password").
+			Input("", input.WithEchoMode(input.EchoPassword))
+		if err != nil {
+			if errors.Is(err, prompt.ErrUserQuit) {
+				os.Exit(1)
+			}
+			return "", err
 		}
-		return "", err
-	}
 
-	if recordPass != repeatRecordPass {
-		return "", ErrPasswordsDontMatch
+		repeatRecordPass, err = prompt.New().Ask("Repeat password").
+			Input("", input.WithEchoMode(input.EchoPassword))
+		if err != nil {
+			if errors.Is(err, prompt.ErrUserQuit) {
+				os.Exit(1)
+			}
+			return "", err
+		}
+
+		if recordPass != repeatRecordPass {
+			fmt.Println(color.InCyan(passwordsDontMatchMsg))
+		}
 	}
 
 	return recordPass, nil
